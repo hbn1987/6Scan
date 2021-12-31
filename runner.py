1249b1544e97656fda3e0545a5f5ac59eba8b83d
root <bingnanhou@163.com>
2021-12-31 05:20:36 +0000

    runner

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(CURRENT_DIR)
 
 if __name__ == "__main__":
-    command = f"{CURRENT_DIR}/6scan -t ICMP6 -I eth0 -s 6Scan"
+    command = f"{CURRENT_DIR}/6scan -t UDP6 -I eth0 -s 6Scan"
     os.system(command)
-    command = f"{CURRENT_DIR}/6scan -t ICMP6 -I eth0 -s 6Gen"
+    command = f"{CURRENT_DIR}/6scan -t UDP6 -I eth0 -s 6Gen"
     os.system(command)
