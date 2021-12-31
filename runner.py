fdf5c253b9c005f47441737925441130b790ee53
root <bingnanhou@163.com>
2021-12-31 03:13:04 +0000

    runner

---
+++ runner.py
@@ -0,0 +1,10 @@
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(CURRENT_DIR)
+
+if __name__ == "__main__":
+    command = f"{CURRENT_DIR}/6scan -t ICMP6 -I eth0 -s 6Scan"
+    os.system(command)
+    command = f"{CURRENT_DIR}/6scan -t ICMP6 -I eth0 -s 6Gen"
+    os.system(command)
